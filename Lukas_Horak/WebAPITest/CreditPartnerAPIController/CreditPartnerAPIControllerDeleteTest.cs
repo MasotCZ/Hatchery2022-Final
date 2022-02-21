5afd6d9d47be7f9450bf85e4ec947c264d508206
Lukáš Horák <lukas.horak.05@gmail.com>
2022-02-21 18:39:32 +0100

    delete tested?

---
+++ Lukas_Horak/WebAPITest/CreditPartnerAPIController/CreditPartnerAPIControllerDeleteTest.cs
@@ -0,0 +1,91 @@
+﻿using AutoMapper;
+using HatcheryFinal_Web_API.Controllers;
+using HatcheryFinal_Web_API.Data.Entities;
+using HatcheryFinal_Web_API.Data.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using NSubstitute.ExceptionExtensions;
+using Shouldly;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace WebAPITest.CreditPartnerAPIController
+{
+    [TestClass]
+    public class CreditPartnerAPIControllerDeleteTest
+    {
+        private CreditPartnerController _controller;
+        private ICreditPartnerRepository _repository;
+        private IMapper _mapper;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _repository = Substitute.For<ICreditPartnerRepository>();
+            _mapper = Substitute.For<IMapper>();
+            _controller = new CreditPartnerController(_repository, _mapper);
+        }
+
+        [TestMethod]
+        public void TestDeleteOk()
+        {
+            //arrange
+            _repository.GetCreditPartnerByIdAsync(0).Returns(new CreditPartner());
+            _repository.SaveChangesAsync().Returns(1);
+
+            //act
+            var res = _controller.Delete(0).Result;
+
+            //assert
+            res.ShouldBeOfType<OkResult>();
+        }
+
+        [TestMethod]
+        public void TestDeleteNotFound()
+        {
+            //arrange
+            _repository.GetCreditPartnerByIdAsync(0).Returns(Task.FromResult<CreditPartner>(null));
+            _repository.SaveChangesAsync().Returns(1);
+
+            //act
+            var res = _controller.Delete(0).Result;
+
+            //assert
+            //Assert.IsTrue(res.Result is NotFoundObjectResult, $"status is: {(res.Result as StatusCodeResult).StatusCode}");
+            res.ShouldBeOfType<NotFoundResult>();
+        }
+
+        [DataRow(0)]
+        [DataRow(2)]
+        [TestMethod]
+        public void TestDeleteSaveChangeError(int changes)
+        {
+            //arrange
+            _repository.GetCreditPartnerByIdAsync(0).Returns(new CreditPartner());
+            _repository.SaveChangesAsync().Returns(changes);
+
+            //act
+            var res = _controller.Delete(0).Result;
+
+            //assert
+            res.ShouldBeOfType<BadRequestObjectResult>();
+        }
+
+        [DataRow(0)]
+        [DataRow(2)]
+        [TestMethod]
+        public void TestDeleteExceptionThrown(int changes)
+        {
+            //arrange
+            _repository.GetCreditPartnerByIdAsync(0).Throws(new System.Exception());
+
+            //act
+            var res = _controller.Delete(0).Result;
+
+            //assert
+            (res as ObjectResult).StatusCode.ShouldBe(StatusCodes.Status500InternalServerError);
+        }
+    }
+}
--- Lukas_Horak/WebAPITest/CreditPartnerAPIController/CreditPartnerAPIControllerPostTest.cs
+++ Lukas_Horak/WebAPITest/CreditPartnerAPIController/CreditPartnerAPIControllerPostTest.cs
@@ -1,17 +1,11 @@
-﻿using AutoMapper;
-using EntityFrameworkCore.Testing.NSubstitute;
-using HatcheryFinal_Web_API.Controllers;
+﻿using EntityFrameworkCore.Testing.NSubstitute;
 using HatcheryFinal_Web_API.Data;
 using HatcheryFinal_Web_API.Data.Dto;
-using HatcheryFinal_Web_API.Data.Entities;
-using HatcheryFinal_Web_API.Data.Repositories;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Console;
 using Microsoft.Extensions.Options;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using NSubstitute;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -30,67 +24,5 @@ namespace WebAPITest.CreditPartnerAPIController
     internal class CreditPartnerAPIControllerPutTest
     {
 
-    }
-
-    [TestClass]
-    internal class CreditPartnerAPIControllerDeleteTest
-    {
-        [TestMethod]
-        public void TestDeleteOk()
-        {
-            //arrange
-            var repository = Substitute.For<ICreditPartnerRepository>();
-            repository.GetCreditPartnerByIdAsync(0).Returns(new CreditPartner());
-            repository.SaveChangesAsync().Returns(1);
-
-            var mapper = Substitute.For<IMapper>();
-
-            var controller = new CreditPartnerController(repository, mapper);
-
-            //act
-            var res = controller.Delete(0);
-
-            //assert
-            Assert.IsTrue(res.Result is OkResult);
-        }
-
-        /*[TestMethod]
-        public async void TestDelete()
-        {
-            var expected = new CreditPartnerFullInfoDto();
-
-            var mockedDbContext = Create.MockedDbContextFor<BankDbContext>();
-            mockedDbContext.Set<CreditPartner>();
-
-            var mapper = Substitute.For<IMapper>();
-            mapper.Map<CreditPartnerFullInfoDto, CreditPartner>(Arg.Any<CreditPartnerFullInfoDto>()).Returns(Arg.Any<CreditPartner>());
-
-            using var loggerFactory = LoggerFactory.Create(builder =>
-            {
-                builder.AddConsole();
-            });
-
-            var logger = loggerFactory.CreateLogger<CreditPartnerRepository>();
-
-            var repository = new CreditPartnerRepository(mockedDbContext, logger);
-
-            var controller = new CreditPartnerController(repository, mapper);
-
-
-
-            var expectedDomainReturn = new DomainItem(0); //Illustrative purposes only
-            mockDomain.Setup(x => x.DomainCall(0)).Returns(expectedDomainReturn); //Illustrative purposes only
-
-            var mockMapper = new Mock<IMapper>();
-            mockMapper.Setup(x => x.Map<DomainItem, ServiceItem>(It.IsAny<DomainItem>()))
-                .Returns(expected);
-
-
-            var service = new Service(mockDomain.Object, mockMapper.Object);
-            var result = service.Get(0);
-
-            mockDomain.Verify(x => x.DomainCall(0), Times.Once);
-            mockMapper.Verify(x => x.Map<DomainItem, ServiceItem>(expectedDomainReturn), Times.Once);
-        }*/
     }
 }
